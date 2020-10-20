839d87b81eebf7bef643b8d0612403f61ef72ebd
Kyle Weekley <krw1619@g.rit.edu>
2020-10-20 15:10:03 -0400

    Created KitchenManager and SelectableObject scripts
    Scripts exist and are incomplete.

---
+++ PAHE/Assets/Scripts/SelectableObject.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectableObject : MonoBehaviour
+{
+    public bool selected;
+    public KitchenManager kitchenManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        selected = false;
+        kitchenManager = GameObject.Find("MainCamera").GetComponent<KitchenManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        // Highlight object if selected
+        if (selected == true)
+        {
+            //TODO: Highlight object edges
+        }
+    }
+
+    // Object is selected when clicked
+    private void OnMouseDown()
+    {
+        // Only select this object if another object is not already selected
+        if (kitchenManager.currentSelection == null)
+        {
+            selected = true;
+            kitchenManager.currentSelection = this.gameObject;
+        }
+    }
+}
