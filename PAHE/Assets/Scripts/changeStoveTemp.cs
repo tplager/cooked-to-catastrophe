aa681c3a3f1deb6cbb5b91c8d63483c1a527ba7d
JohnVance1 <jmv7411@rit.edu>
2020-10-22 11:38:31 -0400

    Added a script that allowed for the changing of the temperature
    Still need to hook it up with the Kitchen Object interactions task to get it fully working. I only did one out of four of the menus as I can just copy paste the same one for the other three.

---
+++ PAHE/Assets/Scripts/changeStoveTemp.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Author: John Vance
+/// Purpose: Allows for each stove burner to be set independently.
+/// Restrictions: Won't fully work until the Kitchen Object Interactions section is finished.
+/// </summary>
+public class changeStoveTemp : MonoBehaviour
+{
+    public int burnerTR = 0;
+    private int burnerBR = 0;
+    private int burnerBL = 0;
+    private int burnerTL = 0;
+
+    #region Getters
+    /// <summary>
+    /// Getter for Top Right burner
+    /// </summary>
+    /// <returns></returns>
+    public int getTR()
+    {
+        return burnerTR;
+
+    }
+
+    /// <summary>
+    /// Getter for Bottome Right burner
+    /// </summary>
+    /// <returns></returns>
+    public int getBR()
+    {
+        return burnerBR;
+
+    }
+
+    /// <summary>
+    /// Getter for Bottom Left burner
+    /// </summary>
+    /// <returns></returns>
+    public int getBL()
+    {
+        return burnerBL;
+
+    }
+
+    /// <summary>
+    /// Getter for Top Left burner
+    /// </summary>
+    /// <returns></returns>
+    public int getTL()
+    {
+        return burnerTL;
+
+    }
+    #endregion
+
+    #region Setters
+    /// <summary>
+    /// Setter for Top Right burner
+    /// </summary>
+    /// <param name="x"></param>
+    public void setTR(int x)
+    {
+        burnerTR = x;
+
+    }
+
+    /// <summary>
+    /// Setter for Bottom Right burner
+    /// </summary>
+    /// <param name="x"></param>
+    public void setBR(int x)
+    {
+        burnerBR = x;
+
+    }
+
+    /// <summary>
+    /// Setter for Bottom Left burner
+    /// </summary>
+    /// <param name="x"></param>
+    public void setBL(int x)
+    {
+        burnerBL = x;
+
+    }
+
+    /// <summary>
+    /// Setter for Top Left burner
+    /// </summary>
+    /// <param name="x"></param>
+    public void setTL(int x)
+    {
+        burnerTL = x;
+
+    }
+    #endregion
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If player touches stove dial
+            // then UI window pops up
+
+        //Get input values from player
+
+
+
+        
+    }
+}
