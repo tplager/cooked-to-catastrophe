985525a33372de3ebebb53b0d595b6243e09e896
trimute2 <ben.j.stern@gmail.com>
2020-11-09 00:41:38 -0500

    added a cart manager class, as well as a cart item class to represent the items in the cart

---
+++ PAHE/Assets/Scripts/CartItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A representation of any item that can be held in a cart
+/// </summary>
+public class CartItem : MonoBehaviour
+{
+	public int Size;
+	public Sprite CartImage;
+}
---
+++ PAHE/Assets/Scripts/CartManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// Author: Ben Stern
+/// <summary>
+/// A singleton that represents the cart, is not destroyed when a new scene is loaded
+/// </summary>
+public class CartManager : MonoBehaviour
+{
+
+	private Dictionary<CartItem,int> ItemsInCart;
+	[SerializeField] private int MaxCartSpace = 50;
+	private int UsedCartSpace;
+
+	private static CartManager _instance;
+
+
+	public static CartManager Instance
+	{
+		get
+		{
+			if(_instance == null)
+			{
+				GameObject obj = new GameObject("CartManager");
+				_instance = obj.AddComponent<CartManager>();
+			}
+			return _instance;
+		}
+	}
+
+	//Im using Awake here because it is called before start
+	private void Awake()
+	{
+		if(_instance != null || _instance != this)
+		{
+			Destroy(gameObject);
+			return;
+		}
+		_instance = this;
+		DontDestroyOnLoad(gameObject);
+	}
+
+	public bool AddItemToCart(CartItem item)
+	{
+		if(UsedCartSpace + item.Size > MaxCartSpace)
+		{
+			Debug.Log("Not Enough Space");
+			return false;
+		}
+		
+		ItemsInCart[item] += 1;
+		UsedCartSpace += item.Size;
+		return true;
+	}
+
+	public bool RemoveItemFromCart(CartItem item)
+	{
+		if(ItemsInCart.ContainsKey(item) || ItemsInCart[item] <= 0)
+		{
+			Debug.Log("Item Not in Cart");
+			return false;
+		}
+
+		ItemsInCart[item] -= 1;
+		UsedCartSpace -= item.Size;
+		return true;
+	}
+
+}
