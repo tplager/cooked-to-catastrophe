13b1e0fd5e64ac5af7b01512739b65e277cd7012
Trenton Plager <tplager1@gmail.com>
2020-11-19 17:36:19 -0500

    Updated documentation for all new code

--- PAHE/Assets/Scripts/CookbookUI.cs
+++ PAHE/Assets/Scripts/CookbookUI.cs
@@ -3,57 +3,75 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI; 
 
+// Author: Trenton Plager
+/// <summary>
+/// Purpose: A script to hold all of the references needed for the Cookbook's UI and contain all of its functionality
+/// Restrictions: None
+/// </summary>
 public class CookbookUI : MonoBehaviour
 {
     #region Fields
+    // The cookbook serializable object
     [SerializeField]
     private Cookbook cookbook;
 
+    // The current page index of the cookbook
     [SerializeField]
     private int currentRecipeIndex = 0;
 
+    // A reference to the text that contains the current recipe's title
     [SerializeField]
     private Text recipeTitle;
 
+    // A reference to the panel that contains all of the ingredient objects
     [SerializeField]
     private GameObject ingredientsPanel;
 
+    // A reference to the panel that contains all of the instruction objects
     [SerializeField]
     private GameObject instructionsPanel;
 
+    // A reference to the prefab used to instantiate more ingredients if there aren't enough
     [SerializeField]
     private GameObject ingredientsPrefab;
 
+    // A reference to the prefab used to instantiate more instructions if there aren't enough
     [SerializeField]
     private GameObject instructionsPrefab;
 
-    [SerializeField]
-    private GameObject content;
-
+    // A reference to the image that indicates whether the recipe is on the specials list or not
     [SerializeField]
     private GameObject isSpecialIndicator;
 
+    // A list of strings that contains all of the specials for the day
     [SerializeField]
     private List<string> specials = new List<string>();
 
+    // A reference to the text object that displays how many of a recipe has been ordered
     [SerializeField]
-    private Text orderQuantity; 
+    private Text orderQuantity;
     #endregion
 
-    // Start is called before the first frame update
+    #region Methods
+    // Author: Trenton Plager
+    /// <summary>
+    /// Purpose: Updates the cookbook display to the current recipe index (which should be 0) 
+    /// Restrcitions: None
+    /// </summary>
     void Start()
     {
+        // This shouldn't need to be here forever
+        // It's just here for testing purposes 
         specials.Add(cookbook.recipes[Random.Range(0, cookbook.recipes.Length)].name);
 
         UpdateCookbookDisplay();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    // Author: Trenton Plager
+    /// <summary>
+    /// Purpose: Goes to the next recipe in the cookbook by advancing the current index and updating the display
+    /// Restrictions: None
+    /// </summary>
     public void NextRecipe()
     {
         if (currentRecipeIndex + 1 < cookbook.recipes.Length && cookbook.recipes[currentRecipeIndex + 1].unlocked)
@@ -67,6 +85,11 @@ public class CookbookUI : MonoBehaviour
         }
     }
 
+    // Author: Trenton Plager
+    /// <summary>
+    /// Purpose: Goes to the previous recipe in the cookbook by decrementing the current index and updating the display
+    /// Restrictions: None
+    /// </summary>
     public void PreviousRecipe()
     {
         if (currentRecipeIndex > 0)
@@ -80,15 +103,28 @@ public class CookbookUI : MonoBehaviour
         }
     }
 
+    // Author: Trenton Plager
+    /// <summary>
+    /// Purpose: Updates the cookbook display by changing the recipe title and updating the ingredients and instructions lists
+    /// Restrictions: None
+    /// </summary>
     public void UpdateCookbookDisplay()
     {
+        // Updates the title on the page
         recipeTitle.text = cookbook.recipes[currentRecipeIndex].name; 
         
+        // Sets the isSpecial indicator if the name of the recipe is contained in the specials list
         isSpecialIndicator.SetActive(specials.Contains(cookbook.recipes[currentRecipeIndex].name));
+
+        // Sets the order quantity
+        // Currently this is random, but it shouldn't always be random
         orderQuantity.text = $"x{Random.Range(0, 5)}"; 
 
+        // Loops through all the ingredients in the recipe
         for (int i = 0; i < cookbook.recipes[currentRecipeIndex].ingredients.Length; i++)
         {
+            // Updates any currently existing ingredient objects in the list display 
+            // Then creates more if necessary
             string ingredient = cookbook.recipes[currentRecipeIndex].ingredients[i];
             try
             {
@@ -105,6 +141,7 @@ public class CookbookUI : MonoBehaviour
             }
         }
 
+        // Sets any ingredient list objects in the display to inactive if they aren't needed
         if (ingredientsPanel.transform.childCount - 1 > cookbook.recipes[currentRecipeIndex].ingredients.Length)
         {
             for (int i = cookbook.recipes[currentRecipeIndex].ingredients.Length + 1; i < ingredientsPanel.transform.childCount; i++)
@@ -113,8 +150,11 @@ public class CookbookUI : MonoBehaviour
             }
         }
 
+        // Loops through all the instructions in the recipe
         for (int i = 0; i < cookbook.recipes[currentRecipeIndex].instructions.Length; i++)
         {
+            // Updates any currently existing instruction objects in the list display 
+            // Then creates more if necessary
             string instruction = cookbook.recipes[currentRecipeIndex].instructions[i];
             try
             {
@@ -131,6 +171,7 @@ public class CookbookUI : MonoBehaviour
             }
         }
 
+        // Sets any instruction list objects in the display to inactive if they aren't needed
         if (instructionsPanel.transform.childCount - 1 > cookbook.recipes[currentRecipeIndex].instructions.Length)
         {
             for (int i = cookbook.recipes[currentRecipeIndex].instructions.Length + 1; i < instructionsPanel.transform.childCount; i++)
@@ -139,4 +180,5 @@ public class CookbookUI : MonoBehaviour
             }
         }
     }
+    #endregion
 }
--- PAHE/Assets/Scripts/JSONRecipeReader.cs
+++ PAHE/Assets/Scripts/JSONRecipeReader.cs
@@ -3,21 +3,38 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
+// Author: Trenton Plager
+/// <summary>
+/// Purpose: Reads in the information from the json file to the Cookbook Scriptable Object
+/// Restrictions: None
+/// </summary>
 public class JSONRecipeReader : MonoBehaviour
 {
     #region Fields
-    public TextAsset jsonFile;
-    public Cookbook cookbook; 
+    // The json file, assigned in the inspector
+    [SerializeField]
+    private TextAsset jsonFile;
+
+    // The cookbook seriazable object, assigned in the inspector
+    [SerializeField]
+    private Cookbook cookbook;
     #endregion
 
-    // Start is called before the first frame update
+    #region Methods
+    // Author: Trenton Plager
+    /// <summary>
+    /// Purpose: Overwrites the current values in the cookbook object on start 
+    ///          So that it matches up with the values in the json file
+    /// Restrictions: None
+    /// </summary>
     void Start()
     {        
         JsonUtility.FromJsonOverwrite(jsonFile.text, cookbook);
 
-        foreach (Recipe recipe in cookbook.recipes)
-        {
-            Debug.Log($"Found Recipe: {recipe.name}");
-        }
+        //foreach (Recipe recipe in cookbook.recipes)
+        //{
+        //    Debug.Log($"Found Recipe: {recipe.name}");
+        //}
     }
+    #endregion
 }
