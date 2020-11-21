27aa0f5c22be6c04a1b38f13eacfed20abf8b4cd
trimute2 <ben.j.stern@gmail.com>
2020-11-20 19:56:27 -0500

    added ability to load a kitchen scene from pantry scene

---
+++ PAHE/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+//Author: Ben Stern
+/// <summary>
+/// A component that allows objects to load other scenes
+/// </summary>
+// Since you cant call static methods in the inspector this object cant be a singleton
+public class SceneLoader : MonoBehaviour
+{
+	public void LoadScene(string s)
+	{
+		SceneManager.LoadScene(s);
+	}
+
+	public void LoadScene(int i)
+	{
+		SceneManager.LoadScene(i);
+	}
+}
