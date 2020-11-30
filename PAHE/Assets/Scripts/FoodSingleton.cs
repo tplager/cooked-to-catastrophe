7489a3c063d70ae1335578b1f247641879082959
JohnVance1 <jmv7411@rit.edu>
2020-11-29 21:09:11 -0500

    Able to transfer food to the cafeteria scene
    Only able to transfer to the cafeteria scene currently and not the other way around

---
+++ PAHE/Assets/Scripts/FoodSingleton.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class FoodSingleton : MonoBehaviour
+{
+    private static FoodSingleton _instance;
+
+    public static FoodSingleton Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject obj = new GameObject("FoodSingleton");
+                _instance = obj.AddComponent<FoodSingleton>();
+            }
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        _instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        OnSceneLoaded(SceneManager.GetActiveScene(), LoadSceneMode.Single);
+
+
+    }
+
+    /// <summary>
+    /// Author: John Vance
+    /// Purpose: Called whenever a scene loads
+    /// Restrictions: None
+    /// </summary>
+    /// <param name="scene">The scene being loaded</param>
+    /// <param name="mode">What mode the scene loads in</param>
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        //food = null;
+        if (scene.name == "Kitchen" || scene.name == "130_Kitchen")
+        {
+            GameObject obj = GameObject.Find("/Canvas/Plate");
+
+            if (obj != null && (obj.transform.childCount > 0))
+            {
+                SetToPlate(obj);
+
+            }
+
+        }
+        if (scene.name == "Cafeteria" || scene.name == "130_Cafeteria")
+        {
+            GameObject obj = GameObject.Find("/Canvas/Serving Plate");
+
+            if (obj != null)
+            {
+                SetToPlate(obj);
+
+
+            }
+
+        }
+    }
+
+
+    public void SetToPlate(GameObject obj)
+    {
+        this.transform.position = obj.transform.position;
+        this.transform.SetParent(obj.transform);
+        Destroy(Instance);
+
+    }
+
+}
---
+++ PAHE/Assets/Scripts/PlateScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+/// <summary>
+/// Author: John Vance
+/// Purpose: Allows for the Plate to detect if food is on it and sets it to be a singleton while on the plate
+/// Restrictions: None 
+/// </summary>
+public class PlateScript : MonoBehaviour
+{
+    private GameObject customFood;
+
+    
+    public void Transfer()
+    {
+        customFood = transform.GetChild(0).gameObject;
+        transform.GetChild(0).SetParent(null);
+
+        customFood.AddComponent<FoodSingleton>();
+
+    }
+}
